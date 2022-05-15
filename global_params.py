977f4de3e09ec653b903e03df599eab525aea027
srishilesh <srishilesh@gmail.com>
2022-05-15 19:25:32 +0530

    Create workspace dir
    Remove cache and pyc files

--- global_params.py
+++ global_params.py
@@ -1,7 +1,8 @@
 import os
 
 ABSOLUTE_PATH = os.path.dirname(os.path.realpath(__file__))
-FRAMES_PATH = 'frames\\dataset2 - 1st group (White car1)'
+# path to dataset image frames
+FRAMES_PATH = 'frames'
 YOLO_OUTPUT_TRACKED_PATH = 'yoloTrackedCoordinates'
 YOLO_OUTPUT_PATH = 'yoloCoordinates'
 LINEAR_INTERPOL_PATH = 'linearInterpolCoordinates'
@@ -16,6 +17,14 @@ FRAME_WW = 1280
 MODEL_HEIGHT = FRAME_HH//6
 MODEL_WIDTH =  FRAME_WW//10
 
-
 STRIP_HEIGHT = FRAME_HH//2
 STRIP_WIDTH =  FRAME_WW//2
+
+
+# Dataset file format conventions
+# FRAMES_PATH = 'frames\\dataset2 - 1st group (White car1)'
+# YOLO_OUTPUT_TRACKED_PATH = 'yoloTrackedCoordinates'
+# YOLO_OUTPUT_PATH = 'yoloCoordinates'
+# LINEAR_INTERPOL_PATH = 'linearInterpolCoordinates'
+# MODEL_B_OUTPUT_PATH = 'modelBCoordinates'
+# FINAL_UI_OUTPUT_PATH = 'finalCoordinates'
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from global_params import *
 
 # Method used to create new folder by checking if it exists before
 def createFolder(name):
-    folder = os.path.join(ABSOLUTE_PATH, name)
+    folder = os.path.join(WORKDIR, name)
     if os.path.exists(folder) == False:
         os.mkdir(folder)
     print("Folder created: " + folder)
 
 # Fetches the absolute path
-ABSOLUTE_PATH = os.path.dirname(os.path.realpath(__file__)) + "\\data"
+WORKDIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'workdir')
 
 # Create folders as specified in global_params
 createFolder(FRAMES_PATH)
@@ -20,12 +20,15 @@ createFolder(FINAL_UI_OUTPUT_PATH)
 createFolder(YOLO_OUTPUT_TRACKED_PATH)
 
 # Write the absolute paths of directories paths.txt 
-with open(ABSOLUTE_PATH + '\\paths.txt','w') as file:
-    file.write(ABSOLUTE_PATH + '\\' + FRAMES_PATH + '\n')
-    file.write(ABSOLUTE_PATH + '\\' + YOLO_OUTPUT_TRACKED_PATH + '\n')
-    file.write(ABSOLUTE_PATH + '\\' + MODEL_B_OUTPUT_PATH + '\n')
-    file.write(ABSOLUTE_PATH + '\\' + LINEAR_INTERPOL_PATH + '\n')
-    file.write(ABSOLUTE_PATH + '\\' + FINAL_UI_OUTPUT_PATH + '\n')
-    file.write(ABSOLUTE_PATH + '\\' + YOLO_OUTPUT_PATH + '\n')
+with open(os.path.join(WORKDIR, 'paths.txt'),'w') as file:
+    file.write(WORKDIR + '/' + FRAMES_PATH + '\n')
+    file.write(WORKDIR + '/' + YOLO_OUTPUT_TRACKED_PATH + '\n')
+    file.write(WORKDIR + '/' + MODEL_B_OUTPUT_PATH + '\n')
+    file.write(WORKDIR + '/' + LINEAR_INTERPOL_PATH + '\n')
+    file.write(WORKDIR + '/' + FINAL_UI_OUTPUT_PATH + '\n')
+    file.write(WORKDIR + '/' + YOLO_OUTPUT_PATH + '\n')
 
 print("paths.txt file created")
+print("Please ensure that you paste the dataset frames into the '/frames' folder")
+
+# cp -r Sample_workdir/frames/dataset2\ -\ 1st\ group\ \(White\ car1\)/* workdir/frames/
